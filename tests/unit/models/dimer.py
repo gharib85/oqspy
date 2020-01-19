1743167bd135e446a83078ea976d8545422e3562
Archie Hewitt <hewitt.archie@yandex.ru>
2020-01-19 07:25:59 +0300

    Add some more dimer functions

--- tests/unit/models/dimer.py
+++ tests/unit/models/dimer.py
@@ -1,8 +1,14 @@
 import unittest
 from tests.definitions import ROOT_DIR
-from oqspy.infrastructure.load import load_sparse_matrix
-from oqspy.models.dimer import dimer_get_sys_size, dimer_get_hamiltonian
+from tests.infrastructure.load import load_sparse_matrix
+from oqspy.models.dimer import \
+    dimer_get_sys_size,\
+    dimer_get_hamiltonian,\
+    dimer_get_periods, \
+    dimer_get_driving_hamiltonias, \
+    dimer_get_driving_functions
 from scipy.sparse.linalg import norm as sps_mtx_norm
+import numpy as np
 
 
 class DimerModel:
@@ -21,8 +27,8 @@ class DimerModel:
 
             self.drv_type = 1
             self.drv_ampl = 3.4
-            self.drv_phas = 1.0
-            self.drv_freq = 0.0
+            self.drv_freq = 1.0
+            self.drv_phas = 0.0
         else:
             self.E = 1.0
             self.U = 0.5
@@ -30,8 +36,8 @@ class DimerModel:
 
             self.drv_type = 0
             self.drv_ampl = 1.5
-            self.drv_phas = 1.0
-            self.drv_freq = 0.0
+            self.drv_freq = 1.0
+            self.drv_phas = 0.0
 
     def get_path(self):
         path = f'{ROOT_DIR}/fixtures/models/dimer/'
@@ -42,7 +48,7 @@ class DimerModel:
         suffix = f'_np({self.num_particles})' + \
                  f'_diss({self.diss_type}_{self.diss_gamma:{precision}})' + \
                  f'_prm({self.E:{precision}}_{self.U:{precision}}_{self.J:{precision}})' + \
-                 f'_drv({self.diss_type}_{self.drv_ampl:{precision}}_{self.drv_phas:{precision}}_{self.drv_freq:{precision}})' + \
+                 f'_drv({self.diss_type}_{self.drv_ampl:{precision}}_{self.drv_freq:{precision}}_{self.drv_phas:{precision}})' + \
                  '.txt'
         return suffix
 
@@ -60,6 +66,11 @@ class TestDimerModel(unittest.TestCase):
         sys_size = dimer_get_sys_size(self.dimer_1.num_particles)
         self.assertEqual(self.dimer_1.sys_size, sys_size)
 
+    def test_period_correctness(self):
+        period_expected = [2.0 * np.pi / self.dimer_1.drv_freq]
+        period_actual = dimer_get_periods(self.dimer_1.drv_freq)
+        self.assertListEqual(period_expected, period_actual)
+
     def test_hamiltonian_correctness(self):
         fn = self.dimer_1.get_path() + 'hamiltonian_mtx' + self.dimer_1.get_suffix()
         h_expected = load_sparse_matrix(fn, self.dimer_1.sys_size)
@@ -71,3 +82,28 @@ class TestDimerModel(unittest.TestCase):
         )
         norm_diff = sps_mtx_norm(h_expected - h_actual)
         self.assertLess(norm_diff, 1.0e-14)
+
+    def test_driving_hamiltonians_correctness(self):
+        fn = self.dimer_1.get_path() + 'hamiltonian_drv_mtx' + self.dimer_1.get_suffix()
+        h_expected = load_sparse_matrix(fn, self.dimer_1.sys_size)
+        hs_actual = dimer_get_driving_hamiltonias(
+            self.dimer_1.num_particles,
+        )
+        norm_diff = sps_mtx_norm(h_expected - hs_actual[0])
+        self.assertLess(norm_diff, 1.0e-14)
+
+    def test_driving_functions(self):
+        period = dimer_get_periods(self.dimer_1.drv_freq)[0]
+        times = list(np.linspace(start=0.0, stop=period, num=11))
+        drv_expected = []
+        drv_actual = []
+        driving_function = dimer_get_driving_functions(
+            self.dimer_1.drv_type,
+            self.dimer_1.drv_ampl,
+            self.dimer_1.drv_freq,
+            self.dimer_1.drv_phas
+        )[0]
+        for time in times:
+            drv_expected.append(self.dimer_1.drv_ampl * np.sin(self.dimer_1.drv_freq * time + self.dimer_1.drv_phas))
+            drv_actual.append(driving_function(time))
+        self.assertListEqual(drv_expected, drv_actual)
